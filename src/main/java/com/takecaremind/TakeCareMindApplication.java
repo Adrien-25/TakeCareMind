f5314d1818a519ca1d13bff1688972760a9fd0c4
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2025-01-21 17:51:11 +0100

    Add Sping starter

---
+++ src/main/java/com/takecaremind/TakeCareMindApplication.java
@@ -0,0 +1,13 @@
+package com.takecaremind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeCareMindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeCareMindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takecaremind/TakeCareMindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takecaremind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeCareMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
